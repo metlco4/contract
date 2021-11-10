32f18062b4eb5058d38d7602b1dd852a3915e424
mackbowes <mackenziepbowes@gmail.com>
2021-11-10 07:05:31 -0800

    added sick ascii art and comments

--- contracts/METL.sol
+++ contracts/METL.sol
@@ -1,6 +1,22 @@
 //SPDX-License-Identifier: Unlicensed
 pragma solidity ^0.8.0;
 
+////////////////////////////////////////////////////////////////////////////////////////
+//      ...     ..      ..           ..      .         .....                ...       //
+//    x*8888x.:*8888: -"888:      x88f` `..x88. .>  .H8888888h.  ~-.    .zf"` `"tu    //
+//   X   48888X `8888H  8888    :8888   xf`*8888%   888888888888x  `>  x88      '8N.  //
+//  X8x.  8888X  8888X  !888>  :8888f .888  `"`    X~     `?888888hx~  888k     d88&  //
+//  X8888 X8888  88888   "*8%- 88888' X8888. >"8x  '      x8.^"*88*"   8888N.  @888F  //
+//  '*888!X8888> X8888  xH8>   88888  ?88888< 888>  `-:- X8888x        `88888 9888%   //
+//    `?8 `8888  X888X X888>   88888   "88888 "8%        488888>         %888 "88F    //
+//    -^  '888"  X888  8888>   88888 '  `8888>         .. `"88*           8"   "*h=~  //
+//     dx '88~x. !88~  8888>   `8888> %  X88!        x88888nX"      .   z8Weu         //
+//   .8888Xf.888x:!    X888X.:  `888X  `~""`   :    !"*8888888n..  :   ""88888i.   Z  //
+//  :""888":~"888"     `888*"     "88k.      .~    '    "*88888888*   "   "8888888*   //
+//      "~'    "~        ""         `""*==~~`              ^"***"`          ^"**""    //
+//                                                                                    //
+////////////////////////////////////////////////////////////////////////////////////////
+
 import "hardhat/console.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
@@ -8,6 +24,11 @@ import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlEnumerableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 
+/**
+ * @title ERC20 token for Metl by RaidGuild
+ *
+ * @author mpbowes, dcoleman, mkdir
+ */
 contract METL is
   Initializable,
   ERC20Upgradeable,
@@ -15,26 +36,48 @@ contract METL is
   PausableUpgradeable,
   AccessControlEnumerableUpgradeable
 {
+  // Role for minters
   bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+  // Role for burners
   bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+
+  // Role for freezers
   bytes32 public constant FREEZER_ROLE = keccak256("FREEZER_ROLE");
+
+  // Role for frozen users
   bytes32 public constant FROZEN_USER = keccak256("FROZEN_USER");
+
+  // Role for pausers
   bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
-  bytes32 public constant SENDER_ROLE = keccak256("SENDER_ROLE");
+
+  // Emit event for role changes
   event RoleChange(address wallet, string role, bool wasAdded);
+
+  // Emit event for pooladdress changes
   event PoolChange(address newPool);
+
+  // Address for mint/burn pool
   address public poolAddress;
 
+  /**
+   * @notice Initializes contract and sets state variables
+   * Note: no params, just assigns deployer to default_admin_role
+   */
   function initialize() public initializer {
     __ERC20_init("METL Coin", "METL");
     __ERC20Burnable_init();
     __Pausable_init();
     __AccessControl_init();
-    poolAddress = 0x0000000000000000000000000000000000000000;
+    poolAddress = address(0);
     _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     _setRoleAdmin(FROZEN_USER, FREEZER_ROLE);
   }
 
+  /**
+   * @notice Admins may update the shared pool address
+   * @param newAddress address of new pool
+   */
   function changePoolAddress(address newAddress)
     external
     onlyRole(DEFAULT_ADMIN_ROLE)
@@ -43,11 +86,20 @@ contract METL is
     emit PoolChange(newAddress);
   }
 
+  /**
+   * @notice Admins may add other admins
+   * @param newAddress address of new admin
+   */
+
   function addAdmin(address newAddress) external onlyRole(DEFAULT_ADMIN_ROLE) {
     grantRole(DEFAULT_ADMIN_ROLE, newAddress);
     emit RoleChange(newAddress, "Admin", true);
   }
 
+  /**
+   * @notice Admins may revoke other admins
+   * @param oldAddress address of admin to revoke
+   */
   function removeAdmin(address oldAddress)
     external
     onlyRole(DEFAULT_ADMIN_ROLE)
@@ -56,11 +108,19 @@ contract METL is
     emit RoleChange(oldAddress, "Admin", false);
   }
 
+  /**
+   * @notice Admins may add new minters
+   * @param newAddress address to grant minter role to
+   */
   function addMinter(address newAddress) external onlyRole(DEFAULT_ADMIN_ROLE) {
     grantRole(MINTER_ROLE, newAddress);
     emit RoleChange(newAddress, "Minter", true);
   }
 
+  /**
+   * @notice Admins may revoke minters
+   * @param oldAddress address of minter to revoke
+   */
   function removeMinter(address oldAddress)
     external
     onlyRole(DEFAULT_ADMIN_ROLE)
@@ -69,11 +129,19 @@ contract METL is
     emit RoleChange(oldAddress, "Minter", false);
   }
 
+  /**
+   * @notice Admins may add new burners
+   * @param newAddress address to grant burner role
+   */
   function addBurner(address newAddress) external onlyRole(DEFAULT_ADMIN_ROLE) {
     grantRole(BURNER_ROLE, newAddress);
     emit RoleChange(newAddress, "Burner", true);
   }
 
+  /**
+   * @notice Admins may revoke burners
+   * @param oldAddress address of burner to revoke
+   */
   function removeBurner(address oldAddress)
     external
     onlyRole(DEFAULT_ADMIN_ROLE)
@@ -82,7 +150,10 @@ contract METL is
     emit RoleChange(oldAddress, "Burner", false);
   }
 
-  // EDIT FREEZERS
+  /**
+   * @notice Admins may add new freezers
+   * @param newAddress address to grant freezer role
+   */
   function addFreezer(address newAddress)
     external
     onlyRole(DEFAULT_ADMIN_ROLE)
@@ -91,6 +162,10 @@ contract METL is
     emit RoleChange(newAddress, "Freezer", true);
   }
 
+  /**
+   * @notice Admins may revoke freezers
+   * @param oldAddress address of freezer to revoke
+   */
   function removeFreezer(address oldAddress)
     external
     onlyRole(DEFAULT_ADMIN_ROLE)
@@ -99,23 +174,37 @@ contract METL is
     emit RoleChange(oldAddress, "Freezer", false);
   }
 
-  // EDIT FROZEN USERS
+  /**
+   * @notice Freezers may 'freeze' users
+   * @param newAddress address to freeze
+   */
   function freezeUser(address newAddress) external onlyRole(FREEZER_ROLE) {
     grantRole(FROZEN_USER, newAddress);
     emit RoleChange(newAddress, "Frozen", true);
   }
 
+  /**
+   * @notice Freezers may 'unfreeze' users
+   * @param oldAddress address to unfreeze
+   */
   function unfreezeUser(address oldAddress) external onlyRole(FREEZER_ROLE) {
     revokeRole(FROZEN_USER, oldAddress);
     emit RoleChange(oldAddress, "Frozen", false);
   }
 
-  // EDIT PAUSERS
+  /**
+   * @notice Admins may add new pausers
+   * @param newAddress address to grant pauser role
+   */
   function addPauser(address newAddress) external onlyRole(DEFAULT_ADMIN_ROLE) {
     grantRole(PAUSER_ROLE, newAddress);
     emit RoleChange(newAddress, "Pauser", true);
   }
 
+  /**
+   * @notice Admins may revoke pausers
+   * @param oldAddress address of pauser to revoke
+   */
   function removePauser(address oldAddress)
     external
     onlyRole(DEFAULT_ADMIN_ROLE)
@@ -124,25 +213,30 @@ contract METL is
     emit RoleChange(oldAddress, "Pauser", false);
   }
 
-  // MINT
+  /**
+   * @notice Minters may mint tokens to a pool
+   * @param amount how many tokens to mint
+   */
   function poolMint(uint256 amount) external onlyRole(MINTER_ROLE) {
-    require(
-      poolAddress != 0x0000000000000000000000000000000000000000,
-      "METL Pool not set"
-    );
+    require(poolAddress != address(0), "METL Pool not set");
     _mint(poolAddress, amount);
   }
 
-  // BURN
+  /**
+   * @notice Burners may burn tokens from a pool
+   * @param amount how many tokens to burn
+   */
   function poolBurn(uint256 amount) external onlyRole(BURNER_ROLE) {
-    require(
-      poolAddress != 0x0000000000000000000000000000000000000000,
-      "METL Pool not set"
-    );
+    require(poolAddress != address(0), "METL Pool not set");
     _burn(poolAddress, amount);
   }
 
-  // TRANSFER (and block frozen wallets)
+  /**
+   * @notice Require users to be unfrozen before allowing a transfer
+   * @param sender address tokens will be deducted from
+   * @param recipient address tokens will be registered to
+   * @param amount how many tokens to send
+   */
   function _beforeTokenTransfer(
     address sender,
     address recipient,
@@ -153,24 +247,29 @@ contract METL is
     super._beforeTokenTransfer(sender, recipient, amount);
   }
 
-  // SEND
+  /**
+   * @notice Admins may send tokens from a pool
+   * @param recipient address tokens will be registered to
+   * @param amount how many tokens to send
+   */
   function poolTransfer(address recipient, uint256 amount)
     public
     onlyRole(DEFAULT_ADMIN_ROLE)
   {
-    require(
-      poolAddress != 0x0000000000000000000000000000000000000000,
-      "METL Pool not set"
-    );
+    require(poolAddress != address(0), "METL Pool not set");
     transferFrom(poolAddress, recipient, amount);
   }
 
-  // PAUSE
+  /**
+   * @notice Pausers may pause the network
+   */
   function pause() public onlyRole(PAUSER_ROLE) {
     _pause();
   }
 
-  // UNPAUSE
+  /**
+   * @notice Pausers may unpause the network
+   */
   function unpause() public onlyRole(PAUSER_ROLE) {
     _unpause();
   }
